fe7510c963020ef9c76b384c9b538b28ec52a79a
RajeshPandey10 <imrajesh2005@gmail.com>
2025-12-29 07:17:31 +0545

    starter...assests from js mastery

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <Button className='cursor-pointer'>click me</Button>
+    </div>
+  )
+}
+
+export default page
